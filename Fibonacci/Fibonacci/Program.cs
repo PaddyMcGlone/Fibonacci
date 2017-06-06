7e07971918255d7a13d0cb05cd9c3a163958cf2b
Paddy Mc Glone <patrick.mcglone@civica.co.uk>
2017-06-06 09:56:54 +0100

    Adding Fibonacci console app

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        public static int Fibonacci(int n)
+        {
+            var a = 0;
+            var b = 1;
+
+            // Work out Fibonacci iteratively
+            for (var i = 0; i < n; i++)
+            {
+                var temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine(Fibonacci(i));
+            }
+        }
+    }
+}
